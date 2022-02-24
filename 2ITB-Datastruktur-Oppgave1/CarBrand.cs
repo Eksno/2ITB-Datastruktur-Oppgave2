e9d8de80cb0fe5104469ffd81ca6aa28be135f60
Jonas. L <33558637+Eksno@users.noreply.github.com>
2022-02-24 10:06:58 +0100

    Add stuff

---
+++ 2ITB-Datastruktur-Oppgave1/CarBrand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ITB_Datastruktur_Oppgave1
+{
+    public class CarBrand : Label
+    {
+        public void Setup(Form form, string initialText, Point initialLocation)
+        {
+            this.Text = initialText;
+            this.Location = initialLocation;
+
+            // Set the AutoSize property of the Label control
+            form.Controls.Add(this);
+        }
+    }
+}
---
+++ 2ITB-Datastruktur-Oppgave1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace _2ITB_Datastruktur_Oppgave1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputField = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblWarn = new System.Windows.Forms.Label();
+            this.lblDebug = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInputField
+            // 
+            this.txtInputField.Location = new System.Drawing.Point(412, 186);
+            this.txtInputField.Name = "txtInputField";
+            this.txtInputField.Size = new System.Drawing.Size(179, 23);
+            this.txtInputField.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(412, 215);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(86, 48);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(505, 215);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(86, 48);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lblWarn
+            // 
+            this.lblWarn.AutoSize = true;
+            this.lblWarn.BackColor = System.Drawing.SystemColors.Control;
+            this.lblWarn.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblWarn.Location = new System.Drawing.Point(412, 266);
+            this.lblWarn.Name = "lblWarn";
+            this.lblWarn.Size = new System.Drawing.Size(0, 15);
+            this.lblWarn.TabIndex = 4;
+            // 
+            // lblDebug
+            // 
+            this.lblDebug.AutoSize = true;
+            this.lblDebug.Location = new System.Drawing.Point(431, 48);
+            this.lblDebug.Name = "lblDebug";
+            this.lblDebug.Size = new System.Drawing.Size(0, 15);
+            this.lblDebug.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDebug);
+            this.Controls.Add(this.lblWarn);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInputField);
+            this.Name = "Form1";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtInputField;
+        private Button btnAdd;
+        private Button btnRemove;
+        private Label lblWarn;
+        private Label lblDebug;
+    }
+}
